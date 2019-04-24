687a568cbe943d686591de6c5c3c2b2d10973e48
Olalekan Babawale <oebabawale@rocketmail.com>
2019-04-24 07:43:09 +0000

    Commited local changes

---
+++ payment_voucher/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Payment Voucher",
+    'author': "Olalekan Babawale",
+    'description': """
+    """,
+    'summary': "",
+    'version': "1.0",
+    'installable': True,
+    'auto_install': False,
+    'depends': [
+        'account_accountant',
+    ],
+    'data': [
+        'reports/payment_templates.xml',
+    ],
+}
