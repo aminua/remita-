56ac549d70c7e01f2b7f354486258f777944d7b6
Olalekan Babawale <padinality@yahoo.com>
2019-11-17 16:09:58 +0100

    Add dependency in website_sale

--- payment_remita/__manifest__.py
+++ payment_remita/__manifest__.py
@@ -7,7 +7,7 @@
     'version': '1.0',
     'description': """Remita Payment Acquirer""",
     'author': 'MgB Computers',
-    'depends': ['website', 'payment', 'website_payment'],
+    'depends': ['website', 'payment', 'website_payment', 'website_sale'],
     'data': [
         'views/remita.xml',
         'views/payment_acquirer.xml',
