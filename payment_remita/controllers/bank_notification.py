5dc1f530c112ca8dca90f2083bee8580ea165ce8
Olalekan Babawale <padinality@yahoo.com>
2019-11-21 16:23:54 +0100

    Fix sales order create invoice state

--- payment_remita/controllers/bank_notification.py
+++ payment_remita/controllers/bank_notification.py
@@ -7,45 +7,37 @@ from odoo.http import request
 class BankPaymentNotification(http.Controller):
     @http.route('/payment/remita/bank', type="http", methods=["POST"], auth="public", csrf=False)
     def bank_payment_feedback(self, **kwargs):
-
         response_dict = {
             'txn_state': "01",
             "message": "Not OK"
         }
         RRR = kwargs.get('rrr')
         data = request.env['payment.transaction']._get_transaction_status(dict(RRR=RRR), acquirer='remita')
-
         request.env['payment.transaction'].sudo().form_feedback(data, 'remita')
-        payment_tx = request.env['payment.transaction'].search([('reference', '=', kwargs.get('orderRef'))])
-
+        payment_tx = request.env['payment.transaction'].sudo().search([('reference', '=', kwargs.get('orderRef'))])
         sale_order = payment_tx.sale_order_id  # TODO: validate sales order if it is not auto-validated
-
         if sale_order.state == 'draft':
             sale_order.action_confirm()
-
         invoice = request.env['account.invoice'].sudo().search([('origin', '=', str(kwargs.get('orderRef')))])
-
         if not invoice.exists():  # if invoice doesnt exist create a new one
-            invoice = self.create_invoice(kwargs)
-
+            invoice = self.create_invoice(payment_tx, sale_order)
+        sale_order.invoice_status = 'invoiced'
         if data.get('status') == "success":  # if payment status is success create and post payment immediately
             # validate the invoice
             invoice.sudo().action_invoice_open()
             # create and post payment immediately
-            payment = self.create_payment(payment_tx, invoice)
+            payment = request.sudo().create_payment(payment_tx, invoice)
             payment and payment.post()
             # create tnx record or verify this is automatically created
             response_dict['txn_state'] = '00'
             response_dict['message'] = 'Ok'
-
         # TODO: reset the sales order on the website to clear the cart.
-        request.website.sale_reset()
         return request.make_response(data=json.dumps(response_dict), headers={
             'content-type': 'application/json; charset=UTF-8'
         }, cookies=None)
 
     @api.multi
-    def create_invoice(self, payment_tx, response_data):
+    def create_invoice(self, payment_tx, sale_order):
         invoice_id = request.env['account.invoice'].sudo().create({
             'journal_id': 1,
             'partner_id': payment_tx.sale_order_id.partner_id.id,  # get from sales order
@@ -81,7 +73,5 @@ class BankPaymentNotification(http.Controller):
             'currency_id': payment_tx.currency_id.id,
             'communication': invoice_id.number
         }
-
         payment_id = request.env['account.payment'].sudo().create(payment_values)
-        payment_id.post()  # post the payment and find a way to connect with invoice
-        return True
+        return payment_id
